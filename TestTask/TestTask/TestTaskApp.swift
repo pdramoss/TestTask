510ef6557715831a09041e4eee9b432183b4b5ff
Pedro Ramos <pdramoss@gmail.com>
2025-03-15 18:05:23 -0500

    Create Xcode Project

---
+++ TestTask/TestTask/TestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTaskApp.swift
+//  TestTask
+//
+//  Created by Pedro Ramos on 15/03/25.
+//
+
+import SwiftUI
+
+@main
+struct TestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
